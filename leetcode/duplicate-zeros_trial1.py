ac3bc97ffeb4330f837626f723ba4f9b62912f46
BethelSahle <bethelsahle00@gmail.com>
2026-03-10 22:49:51 +0300

    Add solution for Duplicate Zeros (trial 1)

---
+++ leetcode/duplicate-zeros_trial1.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def duplicateZeros(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: None Do not return anything, modify arr in-place instead.
+        """
+        n = len(arr)
+        zeros = arr.count(0)
+        i = n - 1
+        while i >= 0:
+            if i + zeros < n:
+                arr[i + zeros] = arr[i]
+            
+            if arr[i] == 0:
+                zeros -= 1
+                if i + zeros < n:
+                    arr[i + zeros] = 0
+            
+            i -= 1
