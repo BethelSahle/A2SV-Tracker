5678ed280854663465e8c093c020a9ee1818d66f
BethelSahle <bethelsahle00@gmail.com>
2026-04-24 22:56:41 +0300

    Add solution for Regular Expression Matching (trial 1)

---
+++ leetcode/regular-expression-matching_trial1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        def dp(i, j):
+            if j == len(p):
+                return i == len(s)
+
+            first_match = i < len(s) and (p[j] == s[i] or p[j] == '.')
+
+            if j + 1 < len(p) and p[j + 1] == '*':
+                return (
+                    dp(i, j + 2) or
+                    (first_match and dp(i + 1, j))
+                )
+
+            return first_match and dp(i + 1, j + 1)
+
+        return dp(0, 0)
